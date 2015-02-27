3be2589efdd7e4bc7d9e1507974adc4cedc1b390
Krzysztof Simon <simon3ti@gmail.com>
2015-02-27 07:07:17 +0100

    poprawienie funkcji wysyłania

--- Procesor/Atmega88/GccApplication4/GccApplication4/GccApplication4.c
+++ Procesor/Atmega88/GccApplication4/GccApplication4/GccApplication4.c
@@ -13,13 +13,21 @@
 #define MYUBRR FOSC/16/BAUD-1
 
 void USART_Init(unsigned int ubrr);
+void USART_Transmit(unsigned char data );
+unsigned char USART_Receive(void);
+void USART_Transmit_buff(const char *buff );
 
 int main(void)
 {
+	unsigned char c;
+	USART_Init(MYUBRR);
+	USART_Transmit_buff("Witam podczas proby wysylania\n");
     while(1)
-	USART_Init(MYUBRR)
     {
-        //TODO:: Please write your application code 
+         c=USART_Receive();
+		 USART_Transmit(c);
+		 USART_Transmit_buff("\n");
+		 
     }
 }
 
@@ -32,4 +40,38 @@ void USART_Init( unsigned int ubrr)
 	UCSR0B = (1<<RXEN0)|(1<<TXEN0);
 	/* Set frame format: 8data, 1stop bit */
 	UCSR0C = (0<<USBS0)|(3<<UCSZ00);
+}
+
+void USART_Transmit(unsigned char data )
+{
+	/*
+	Wait for empty transmit buffer
+	*/
+	while( !( UCSR0A & (1<<UDRE0)));
+	/*
+	Put data into buffer, sends the data
+	*/
+	UDR0 = data;
+}
+
+void USART_Transmit_buff(const char *buff ){
+	
+	while(*(buff)){
+	USART_Transmit(*(buff));
+	buff++;	
+	}
+	
+}
+
+unsigned char USART_Receive(void)
+{
+	/*
+	Wait for data to be received
+	*/
+	while
+	( !(UCSR0A & (1<<RXC0)) );
+	/*
+	Get and return received data from buffer
+	*/
+	return UDR0;
 }
